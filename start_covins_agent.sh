262fccabc124bf53443db751306a17f08bdc8b34
v1outsc <v1otusc@yeah.net>
2020-11-02 17:36:13 +0800

    添加开机启动脚本

---
+++ start_covins_agent.sh
@@ -0,0 +1,15 @@
+#！/bin/bash
+# open mynteye camera: modified file path plz
+cd /home/nvidia/MYNT-EYE-S-SDK-master/wrappers/ros/
+source ./devel/setup.bash
+{
+  gnome-terminal -x bash -c "roslaunch mynt_eye_ros_wrapper mynteye.launch"
+}&
+
+sleep 10s
+# launch covins agent.launch: modified file path plz
+cd /home/nvidia/covins/
+source ./devel/setup.bash
+{
+  gnome-terminal -x bash -c "roslaunch vins_estimator agent.launch;exec bash"
+}&
---
+++ start_covins_master.sh
@@ -0,0 +1,15 @@
+#！/bin/bash
+# open mynteye camera: modified file path plz
+cd /home/nvidia/MYNT-EYE-S-SDK-master/wrappers/ros/
+source ./devel/setup.bash
+{
+  gnome-terminal -x bash -c "roslaunch mynt_eye_ros_wrapper mynteye.launch"
+}&
+
+sleep 10s
+# launch covins master.launch: modified file path plz
+cd /home/nvidia/covins/
+source ./devel/setup.bash
+{
+  gnome-terminal -x bash -c "roslaunch vins_estimator master.launch;exec bash"
+}&
